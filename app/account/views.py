b6e7fe99aafce09a767692872e9addb3e587c7da
Nafasov <nafasovnurxon18@gmail.com>
2024-03-31 15:41:16 +0500

    init

--- app/account/views.py
+++ app/account/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import User
+from .serializers import (
+    UserRegistrationSerializer,
+    UserSerializer
+    )
+
+
+class UserRegistrationAPIView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+    queryset = User.objects.all()
+
+
+class MyProfileAPIView(generics.GenericAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        user_id = request.user.id
+        user = User.objects.get(user_id=user_id)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+
+
+
+
