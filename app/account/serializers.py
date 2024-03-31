f58d193633184f50aea64d93938896ee2d085d22
Nafasov <nafasovnurxon18@gmail.com>
2024-03-31 15:15:50 +0500

    init

---
+++ app/account/serializers.py
@@ -0,0 +1,44 @@
+from cProfile import Profile
+
+from rest_framework.exceptions import ValidationError
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+
+from .models import User
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(min_length=8, validators=[validate_password])
+    password2 = serializers.CharField(min_length=8, validators=[validate_password])
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'avatar', 'last_name', 'first_name', 'password', 'password2', 'created_date']
+
+    def validate_email(self, attrs):
+        password = attrs.get('password')
+        password2 = attrs.get('password2')
+
+        if password != password2:
+            raise ValidationError("Passwords don't match")
+        return attrs
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        password2 = validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'avatar', 'last_name', 'last_login', 'modified_date', 'created_date']
+
+
+
+
+
