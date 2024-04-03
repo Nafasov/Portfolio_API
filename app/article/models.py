783c5a3237eb829f547263436c25e48525922fc5
Nafasov <nafasovnurxon18@gmail.com>
2024-04-03 09:18:02 +0500

    modified

--- app/article/models.py
+++ app/article/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from django.utils.text import slugify
+from django.db.models.signals import pre_save
+from django.utils import timezone
+from app.account.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='articles')
+    tags = models.ManyToManyField(Tag, related_name='articles')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='articles')
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(editable=True)
+    image = models.ImageField(upload_to='article', blank=True)
+    content = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class SubArticle(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='subarticles')
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='article', null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='parents')
+    top_level_comment_id = models.IntegerField(null=True, blank=True)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def children(self):
+        if not self.parent:
+            return Comment.objects.filter(top_level_comment_id=self.id)
+        return None
+
+
+def pre_save_article(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.title+'-'+str(timezone.now().date()))
+
+
+pre_save.connect(pre_save_article, sender=Article)
+
+
+def pre_save_comments(sender, instance, *args, **kwargs):
+    if instance.parent:
+        if instance.parent.top_level_comment_id:
+            instance.top_level_comment_id = instance.parent.top_level_comment_id
+        else:
+            instance.top_level_comment_id = instance.parent.id
+
+
+pre_save.connect(pre_save_comments, sender=Comment)
