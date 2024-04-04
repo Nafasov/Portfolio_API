3f928933178f7511aff0c5d6e03df2273a08bfaf
Nafasov <nafasovnurxon18@gmail.com>
2024-04-04 15:48:05 +0500

    modified

--- app/main/urls.py
+++ app/main/urls.py
@@ -1,10 +1,34 @@
 from django.urls import path
 
+from .views import (
+    ProfessionAPIView,
+    AboutMeAPIView,
+    PartnerAPIView,
+    EducationAPIView,
+    ExperienceAPIView,
+    AwardAPIView,
+    SkillsAPIView,
+    ServicesAPIView,
+    ProjectsAPIView,
+    DoneAPIView,
+    MeContactsAPIView
+    )
 
 app_name = 'main'
 
 
 urlpatterns = [
+    path('professions/', ProfessionAPIView.as_view()),
+    path('aboutme/', AboutMeAPIView.as_view()),
+    path('partner/', PartnerAPIView.as_view()),
+    path('education/', EducationAPIView.as_view()),
+    path('experience/', ExperienceAPIView.as_view()),
+    path('award/', AwardAPIView.as_view()),
+    path('skills/', SkillsAPIView.as_view()),
+    path('services/', ServicesAPIView.as_view()),
+    path('projects/', ProjectsAPIView.as_view()),
+    path('done/', DoneAPIView.as_view()),
+    path('mecontacts/', MeContactsAPIView)
 
 ]
 
